9bfe1565cd62b1ae50c71426420859f8bc22a4c5
Ethan Lee <wxliyc@gmail.com>
2022-04-25 16:21:28 -0500

    commit tree 5049ab532ab62be4bc9b8928f6fede4bc536ca6f

---
+++ app/controllers/spices_controller.rb
@@ -0,0 +1,32 @@
+class SpicesController < ApplicationController
+    def index
+        @spices = Spice.all
+        render json: @spices
+    end
+
+    # def show
+    #     @spice = Spice.find(params[:id])
+    #     render json: @spice
+    # end
+
+    def create
+        @spice = Spice.create(spice_params)
+        render json: @spice, status: :created
+    end
+
+    def update
+        @spice = Spice.find(params[:id])
+        @spice.update(spice_params)
+        render json: @spice
+    end
+
+    def destroy
+        @spice = Spice.find(params[:id])
+        @spice.destroy
+        head :no_content
+    end
+
+    def spice_params
+        params.permit(:title, :description, :price, :image, :notes, :rating)
+    end
+end
